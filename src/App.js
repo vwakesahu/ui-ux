b677919a580ef0f4b8003a9a7be4dfc4ec2d7c5d
vwakesahu <89719144+vwakesahu@users.noreply.github.com>
2023-09-20 21:34:43 +0530

    Done with techstack

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import ProblemStatement from "./components/ProblemStatement";
 import Footer from "./components/Footer";
 import Solution from "./components/Solution";
+import TechStack from "./components/TechStack";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <div className="md:p-12 p-8">
           <ProblemStatement />
           <Solution />
+          <TechStack />
         </div>
         <Footer />
       </main>
